77455e120da8ee24790801eb807b6e521a526ea2
chris martinez <cardamomclouds@yahoo.com>
2016-08-29 11:46:46 -0700

    new method to split asnwer string into an array of letters

--- js/myscripts.js
+++ js/myscripts.js
@@ -7,9 +7,21 @@ function Player(name){
 function AnswerMaker (clue, answer){
   this.clue = clue;
   this.answer = answer;
+  this.hiddenAnswerArray = [];
+
 };
 
+AnswerMaker.prototype.hideAnswer = function(){
+  var answerLetter = this.answer.split("");
+  // answerLetter.map(hiddenAnswerArray.push("_"));
+  //   // a-zA-Z
+  //   console.log("this.hiddenAnswerArray");
+  };
+
+var consonants = ["b","c","d","f","g","h","j","k","l", "m","n","p","q","r","s","t","v","w","x","y","z"];
+var vowels = ["a","e","i","o","u"];
 var answersArray = [];
+
 var answers = [["Food & Drink", "Fish And Chips"],["Pop Songs", "All My Single Ladies"],["Movies", "Gone With the Wind"]];
 
 answers.forEach(function(answer){
