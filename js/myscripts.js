ba87d0a63b3a33570feb6b2d2be45228ff53a256
chris martinez <cardamomclouds@yahoo.com>
2016-08-29 11:11:14 -0700

    create player constructor

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,7 +1,14 @@
+function Player(name){
+  this.name = name;
+  this.score = 0;
+  this.avatar = "";
+};
+
 function AnswerMaker (clue, answer){
   this.clue = clue;
   this.answer = answer;
-}
+};
+
 var answersArray = [];
 var answers = [["Food & Drink", "Fish And Chips"],["Pop Songs", "All My Single Ladies"],["Movies", "Gone With the Wind"]];
 
@@ -9,5 +16,6 @@ answers.forEach(function(answer){
   var newAnswer = new AnswerMaker(answer[0],answer[1]);
   answersArray.push(newAnswer);
 });
+
 
 console.log(answersArray);
