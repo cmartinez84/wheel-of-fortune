3e23b441242321cab51f46688ddb86a9d1bb95bf
Chris Martinez <cardamomclouds@yahoo.com>
2016-08-30 16:31:52 -0700

    fix taking turns problems and change round when player guesses incorrectly

--- js/myscripts.js
+++ js/myscripts.js
@@ -102,6 +102,8 @@ var getRandomAnswer = function(){
    return answersArray[randomNumber];
 };
 
+var randomAnswer = getRandomAnswer();
+
 //User Interface
 $(document).ready(function(){
  var generateBoard = function(randomAnswer){
@@ -118,7 +120,6 @@ $(document).ready(function(){
      } ///display answer on board
    }
  }
-  var randomAnswer = getRandomAnswer();
   // var randomAnswer;
   $("#playerEntryForm").submit(function(event){
     // randomAnswer = getRandomAnswer();
@@ -128,14 +129,9 @@ $(document).ready(function(){
     var player2Name = $("input#player2Input").val();
     player1 = new Player(player1Name);
     player2 = new Player(player2Name);
-    $("#player-one").text(player1Name);
-    $("#player-two").text(player2Name);
-    $("#player-one-score").text(player1.score);
-    $("#player-two-score").text(player2.score);
-    $("#spin").show();
-    $("#playerEntryForm").hide();
     player1Turn();
   });
+
   var player1Spin;
   var player1Turn = function(){
     $("button").off();
@@ -144,52 +140,56 @@ $(document).ready(function(){
     $("#spin").click(function(){
       player1Spin = spin(wheelWedges);
       console.log("this is player 1's spin" + player1Spin)
-      $("#player-one-score").text(player1.score);
-      console.log("1 " + player1Spin);
       if (player1Spin === "Bankrupt"){
         player1.score = 0;
         player2Turn();
       } else if (player1Spin === "Lose Turn"){
         player2Turn();
-      } else {
-        $("#letterEntryForm").show();
-        $("#letterEntryForm").submit(function(event){
-          event.preventDefault();
-          var player1LetterGuess = $("input#letterEntryInput").val();
-          player1LetterGuess = player1LetterGuess.toLowerCase();
-          randomAnswer.letterCheck(player1LetterGuess, player1Spin);
-          player1Turn();
-        });
       }
-    })  ;
+    });
+    $("#enterLetter").click(function(){
+        var player1LetterGuess = $("input#letterEntryInput").val();
+        player1LetterGuess = player1LetterGuess.toLowerCase();
+        var roundScore =randomAnswer.letterCheck(player1LetterGuess, player1Spin);
+        player1.score += roundScore;
+        if(roundScore ===0){
+          player2Turn();
+        }
+        else{
+          player1Turn();
+        }
+
+    });
   }
+
+  ////end player 1
   var player2Spin;
   var player2Turn = function(){
     $("button").off();
-    $("#player-two").toggleClass("Selected");
     $("#player-one").toggleClass("Selected");
+    $("#player-two").toggleClass("Selected");
     $("#spin").click(function(){
-      console.log("this is player 2's spin" + player1Spin)
-
       player2Spin = spin(wheelWedges);
-      $("#player-two-score").text(player2.score);
-      console.log("2 " + player2Spin);
+      console.log("this is player 2's spin" + player2Spin)
       if (player2Spin === "Bankrupt"){
         player2.score = 0;
         player1Turn();
       } else if (player2Spin === "Lose Turn"){
         player1Turn();
-      } else {
-        $("#letterEntryForm").show();
       }
-      $("#letterEntryForm").submit(function(event){
-        event.preventDefault();
+    });
+    $("#enterLetter").click(function(){
         var player2LetterGuess = $("input#letterEntryInput").val();
         player2LetterGuess = player2LetterGuess.toLowerCase();
-
-        randomAnswer.letterCheck(player2LetterGuess, player2Spin);
-        player2Turn();
-      });
+        var roundScore =randomAnswer.letterCheck(player2LetterGuess, player2Spin);
+        player2.score += roundScore;
+        if(roundScore ===0){
+          player1Turn();
+        }
+        else{
+          player2Turn();
+        }
     });
-  }
+  }////end player 1
+
 });
